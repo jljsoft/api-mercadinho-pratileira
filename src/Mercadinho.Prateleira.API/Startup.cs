fe15c4cc401901e02d08554d546a7c7a0b379843
Joao Lucas <lucasjesussoft@gmail.com>
2022-06-11 00:54:09 -0300

    Conexão com banco

--- src/Mercadinho.Prateleira.API/Startup.cs
+++ src/Mercadinho.Prateleira.API/Startup.cs
@@ -1,7 +1,6 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Mercadinho.Prateleira.Infraestrutura.Data.DataRegistration;
+using Microsoft.Extensions.Configuration;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -12,10 +11,17 @@ namespace Mercadinho.Prateleira.API
 {
     public class Startup
     {
+        private readonly IConfiguration _configuration;
+
+        public Startup(IConfiguration _configuration)
+        {
+            this._configuration = _configuration;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDataRegistration(_configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
